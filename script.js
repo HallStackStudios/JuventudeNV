532c81ccc6398eceae9d02fe421de7e4dca0ebf3
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-08-25 23:45:22 -0300

    commit

--- script.js
+++ script.js
@@ -26,7 +26,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     observer.observe(imageContainer);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const topBar = document.getElementById('top-bar');
 
+    // Função para animar o top-bar
+    function showTopBar() {
+        topBar.classList.add('visible2'); // Adiciona a classe 'visible' para o top-bar
+    }
+
+    // Chama a função para exibir o top-bar após um pequeno atraso
+    setTimeout(showTopBar, 500); // Ajuste o tempo de atraso conforme necessário
+});
 // script.js
 
 // script.js
