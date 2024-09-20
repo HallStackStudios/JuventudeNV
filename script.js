3e5d1036e38dc682e0c4c44e1c8066c535dffe5a
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-09-20 10:00:02 -0300

    commit

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const imageContainer = document.querySelector('.use, #img-imagem-ref');
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
-
-    observer.observe(imageContainer);
-});
-document.addEventListener('DOMContentLoaded', function() {
-    const imageContainer = document.querySelector('#img-imagem-rec');
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
-
-    observer.observe(imageContainer);
-});
 document.addEventListener('DOMContentLoaded', () => {
     const topBar = document.querySelector('#top-bar');
 
